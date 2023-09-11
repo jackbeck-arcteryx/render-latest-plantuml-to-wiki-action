62ef25b652478d5db5c69b7449e3bb8a207b6128
Chris Gibbons <christopher.gibbons@rockwellautomation.com>
2023-09-11 15:57:52 -0400

    Pull from latest PlantUML link

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ artifacts_upload_dir=$INPUT_OUTPUT_DIR
 # echo "---"
 
 # Set git user settings (this is needed to commit and push):
-git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.name "$GITHUB_ACTOR"
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 # git config --global user.name "GitHub Action 'Render PlantUML'"
 # git config --global user.email "github-action@users.noreply.github.com"
@@ -38,7 +38,8 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 input_files=$(find "$local_input_dir" -type f -name '*' -print)
 
 echo "=> Downloading PlantUML Java app ..."
-wget --quiet -O plantuml.jar https://sourceforge.net/projects/plantuml/files/plantuml.1.2020.15.jar/download
+# wget --quiet -O plantuml.jar https://sourceforge.net/projects/plantuml/files/plantuml.1.2020.15.jar/download
+wget --quiet -O plantuml.jar https://sourceforge.net/projects/plantuml/files/latest/download
 
 echo "=> Preparing output dir ..."
 mkdir -p "$local_output_dir"
@@ -50,13 +51,12 @@ echo "=> Starting render process ..."
 ORIGINAL_IFS="$IFS"
 IFS='
 '
-for file in $input_files
-do
-    input_filepath=$file
-    output_filepath=$(dirname $(echo $file | sed -e "s@^${local_input_dir}@${local_output_dir}@"))
+for file in "${input_files[@]}"; do
+	input_filepath=$file
+	output_filepath=$(dirname "$(echo "$file" | sed -e "s@^${local_input_dir}@${local_output_dir}@")")
 
-    echo " > processing '$input_filepath'"
-    java -jar plantuml.jar -charset UTF-8 $INPUT_JAVA_ARGS -output "${GITHUB_WORKSPACE}/${output_filepath}" "${GITHUB_WORKSPACE}/${input_filepath}"
+	echo " > processing '$input_filepath'"
+	java -jar plantuml.jar -charset UTF-8 "$INPUT_JAVA_ARGS" -output "${GITHUB_WORKSPACE}/${output_filepath}" "${GITHUB_WORKSPACE}/${input_filepath}"
 done
 IFS="$ORIGINAL_IFS"
 # source: https://unix.stackexchange.com/questions/9496/looping-through-files-with-spaces-in-the-names
@@ -70,11 +70,11 @@ echo "=> Cleaning up possible left-overs from another render step ..."
 rm -r "${GITHUB_WORKSPACE}/artifacts_repo"
 
 echo "=> Cloning wiki repository ..."
-git clone $artifacts_repo "${GITHUB_WORKSPACE}/artifacts_repo"
+git clone "$artifacts_repo" "${GITHUB_WORKSPACE}/artifacts_repo"
 if [ $? -gt 0 ]; then
-    echo "   ERROR: Could not clone repo."
-    echo "   Note: you need to initialize the wiki by creating at least one page before you can use this action!"
-    exit 1
+	echo "   ERROR: Could not clone repo."
+	echo "   Note: you need to initialize the wiki by creating at least one page before you can use this action!"
+	exit 1
 fi
 
 echo "=> Moving generated files to /${artifacts_upload_dir} in wiki repo ..."
@@ -88,14 +88,14 @@ cd "${GITHUB_WORKSPACE}/artifacts_repo"
 git add .
 
 if git commit -m"Auto-generated PlantUML diagrams"; then
-    echo "=> Pushing artifacts ..."
-    git push
-    if [ $? -gt 0 ]; then
-    echo "   ERROR: Could not push to repo."
-    exit 1
-fi
+	echo "=> Pushing artifacts ..."
+	git push
+	if [ $? -gt 0 ]; then
+		echo "   ERROR: Could not push to repo."
+		exit 1
+	fi
 else
-    echo "(i) Nothing changed since previous build. The wiki is already up to date and therefore nothing is being pushed."
+	echo "(i) Nothing changed since previous build. The wiki is already up to date and therefore nothing is being pushed."
 fi
 
 # Print success message:
@@ -109,9 +109,8 @@ output_files=$(find "${GITHUB_WORKSPACE}/artifacts_repo/${artifacts_upload_dir}"
 ORIGINAL_IFS="$IFS"
 IFS='
 '
-for file in $output_files
-do
-    filename=$(basename $file)
-    echo "[[$(echo ${file} | sed -e "s@^${GITHUB_WORKSPACE}/artifacts_repo@@")|alt=${filename%.*}]]"
+for file in "${output_files[@]}"; do
+	filename=$(basename "$file")
+	echo "[[$(echo "$file" | sed -e "s@^${GITHUB_WORKSPACE}/artifacts_repo@@")|alt=${filename%.*}]]"
 done
 IFS="$ORIGINAL_IFS"
